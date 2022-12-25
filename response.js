bbd880b2189031d0525a66c2c81c811f4ad4509e
mushtariyibodillayeva <dilfuzaxamrayeva53@gmail.com>
2022-12-25 19:10:59 +0500

    new

---
+++ response.js
@@ -0,0 +1,9 @@
+const responseObj = {
+  salom: "Assalomu alaykum",
+  hello: "Hello",
+  hey: "nima xizmat",
+  today: new Date().toDateString(),
+  bugun: new Date().toDateString(),
+  time: new Date().toLocaleTimeString(),
+  time: new Date().toLocaleTimeString(),
+};
